22eac49403e324d24815f4b5f55cf4ac974cd853
fyr233 <36994804+fyr233@users.noreply.github.com>
2022-01-30 03:07:11 +0800

    init

---
+++ OpenFMZ.py
@@ -0,0 +1,99 @@
+import time
+import os
+import json
+import sys
+
+OUT_DATA_ROOT = './webpage/static/data/'
+OUT_DATA_INFO = 'info/latest.info'
+INFO = {
+    "start": 0,
+    "file": "py_filename",
+    "logSize": 0,
+    "value": 'value/0.value',
+    "custom": {
+        "file": 'custom/0.custom',
+        "cfgs": {}
+    },
+    "log": 'log/0.log'
+}
+
+class Chart:
+    def __init__(self, cfglist):
+        global INFO
+        self.cfgs = cfglist
+        INFO["custom"]["cfgs"] = cfglist
+        WriteINFO()
+
+    def add(self, seriesId, d):
+        groupId = self.seriesId2groupId(seriesId)
+        with open(OUT_DATA_ROOT + INFO["custom"]["file"], 'a') as f:
+            s = str(groupId) + ' ' +\
+                str(seriesId) + ' ' +\
+                str(d[0]) + ' ' +\
+                str(d[1]) + '\n'
+            f.write(s)
+
+    def reset(self):
+        global INFO
+        nowstr = str(int(time.time()*1000))
+        INFO["custom"]["file"] = 'custom/' + nowstr + '.custom'
+        WriteINFO()
+    
+    def seriesId2groupId(self, seriesId):
+        r = 0
+        for i in range(len(self.cfgs)):
+            for j in range(len(self.cfgs[i]['series'])):
+                if r == seriesId:
+                    return i
+                else:
+                    r += 1
+        return -1
+
+def Log(*arg):
+    now = int(time.time()*1000)
+    with open(OUT_DATA_ROOT + INFO["log"], 'a', encoding='utf8') as f:
+        s = str(now) + ' 信息 ' +\
+            ' '.join([str(i) for i in arg]) + '\n'
+        f.write(s)
+
+def LogProfit(p):
+    now = int(time.time()*1000)
+    with open(OUT_DATA_ROOT + INFO["value"], 'a', encoding='utf8') as f:
+        f.write(str(now) + ' ' + str(p) + '\n')
+    
+    with open(OUT_DATA_ROOT + INFO["log"], 'a', encoding='utf8') as f:
+        s = str(now) + ' 收益 ' +\
+            str(p) + '\n'
+        f.write(s)
+    
+def WriteINFO():
+    with open(OUT_DATA_ROOT + OUT_DATA_INFO, 'w', encoding='utf8') as f:
+        f.write(json.dumps(INFO))
+
+def Sleep(ms):
+    time.sleep(ms/1000)
+
+def N_(v, n):
+    return round(v, n)
+
+def init():
+    now = int(time.time()*1000)
+    nowstr = str(now)
+
+    #备份旧的latest.info文件
+    try:
+        os.rename(OUT_DATA_ROOT + OUT_DATA_INFO, OUT_DATA_ROOT + 'info/' + nowstr + '.info')
+    except:
+        pass
+
+    #写入latest.info
+    back_frame = sys._getframe().f_back
+    back_filename = os.path.basename(back_frame.f_code.co_filename)
+    INFO["start"] = now
+    INFO["file"] = back_filename
+    INFO["value"] = 'value/' + nowstr + '.value'
+    INFO["custom"]["file"] = 'custom/' + nowstr + '.custom'
+    INFO["log"] = 'log/' + nowstr + '.log'
+    WriteINFO()
+
+init()
---
+++ webpage/static/js/draw.js
@@ -0,0 +1,274 @@
+//命名空间
+var DRAW = {};
+//实盘信息
+DRAW.info = {};
+//value数据
+DRAW.valueData = [];
+//custom数据
+DRAW.customData = [];
+//log数据
+DRAW.logData = [];
+
+//估值曲线
+DRAW.Chart_allvalue_curve = echarts.init($('#allvalue_curve')[0], 'chalk');
+DRAW.Chart_allvalue_curve.showLoading();
+DRAW.Chart_allvalue_curve.setOption(option = {
+    tooltip: {
+      trigger: 'axis',
+      formatter: function(params){
+          data = params[0].data
+          return '' + data[0].toLocaleString('chinese',{hour12:false}) + 
+                '<br/>' + 
+                data[1]
+      }
+    },
+    title: {
+      show: false,
+      left: 'center',
+      text: '估值曲线'
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: 'none'
+        },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'time',
+      boundaryGap: false
+    },
+    yAxis: {
+      type: 'value',
+      boundaryGap: [0, '100%']
+    },
+    dataZoom: [
+      {
+        type: 'inside',
+        start: 0,
+        end: 100
+      },
+      {
+        start: 0,
+        end: 20
+      }
+    ],
+    series: [
+      {
+        type: 'line',
+        smooth: false,
+        areaStyle: {},
+        //data: data
+      }
+    ],
+    dataset:{
+        source:DRAW.valueData
+    }
+});
+
+//获取并解析info文件
+DRAW.infoXhr = $.get('../static/data/info/latest.info' + '?' + Math.random()).done(
+    function(data){
+        DRAW.info = JSON.parse(data);
+        DRAW.setInfo();
+        DRAW.getData();
+    }
+).fail(
+    function(xhr, status){
+        console.log('get logfile error: ' + status);
+    }
+);
+
+DRAW.getData = function(){
+    //根据解析的info，获取其它三个文件
+    DRAW.valueXhr = $.get('../static/data/' + DRAW.info.value + '?' + Math.random()).done(
+        function(data){
+            lines = data.split('\n');
+            lines.forEach(line => {
+                words = line.split(' ');
+                if (words.length > 1){
+                    let date = new Date(parseInt(words[0]));
+                    let value = parseFloat(words[1]);
+                    DRAW.valueData.push([date, value]);
+                }
+            });
+
+            DRAW.info.logSize += parseInt(DRAW.valueXhr.getResponseHeader('content-length'));
+            DRAW.setInfo();
+            DRAW.Chart_allvalue_curve.setOption({
+                dataset: {source: DRAW.valueData}
+            });
+            DRAW.Chart_allvalue_curve.hideLoading();
+        }
+    ).fail(
+        function(xhr, status){
+            console.log('get valuefile error: ' + status);
+        }
+    );
+
+    //根据解析的info，获取其它三个文件
+    DRAW.customXhr = $.get('../static/data/' + DRAW.info.custom.file + '?' + Math.random()).done(
+        function(data){
+            lines = data.split('\n');
+            lines.forEach(line => {
+                words = line.split(' ');
+                if (words.length > 3){
+                    let groupId = parseInt(words[0]);
+                    let id = parseInt(words[1]);
+                    let date = new Date(parseInt(words[2]));
+                    let value = parseFloat(words[3]);
+                    DRAW.customData.push([groupId, id, date, value]);
+                }
+            });
+
+            DRAW.info.logSize += parseInt(DRAW.customXhr.getResponseHeader('content-length'));
+            DRAW.setInfo();
+            DRAW.setCustom();
+        }
+    ).fail(
+        function(xhr, status){
+            console.log('get customfile error: ' + status);
+        }
+    );
+
+    //根据解析的info，获取其它三个文件
+    DRAW.logXhr = $.get('../static/data/' + DRAW.info.log + '?' + Math.random()).done(
+        function(data){
+            lines = data.split('\n');
+            lines.forEach(line => {
+                words = line.split(' ');
+                if (words.length > 2){
+                    let date = new Date(parseInt(words[0]));
+                    let type = words[1];
+                    let msg = words.slice(2).join(' ');
+                    DRAW.logData.push([date, type, msg]);
+                }
+            });
+
+            DRAW.info.logSize += parseInt(DRAW.logXhr.getResponseHeader('content-length'));
+            DRAW.setInfo();
+            DRAW.setLog();
+        }
+    ).fail(
+        function(xhr, status){
+            console.log('get logfile error: ' + status);
+        }
+    );
+}
+
+DRAW.setInfo = function(){
+    $('#info_startTime').text('开始于: ' + Date(DRAW.info.start).toLocaleString('chinese',{hour12:false}));
+    $('#info_fileName').text(DRAW.info.file);
+    $('#info_logSize').text('日志大小: ' + Math.round(100 * DRAW.info.logSize / 1024) / 100 + 'KB');
+}
+
+//画多个自定义曲线
+DRAW.setCustom = function(){
+    DRAW.customCharts = [];
+    DRAW.customGroups = [];
+
+    DRAW.customData.forEach(line => {
+        if (DRAW.customGroups[line[0]] == undefined){
+            DRAW.customGroups[line[0]] = [];
+        }
+        if (DRAW.customGroups[line[0]][line[1]] == undefined){
+            DRAW.customGroups[line[0]][line[1]] = [];
+        }
+        DRAW.customGroups[line[0]][line[1]].push([line[2], line[3]]);
+    });
+
+    DRAW.customGroups.forEach((group, i) => {
+        
+        if (group != undefined){
+            var chartDiv = $('<div class="custom_curve"></div>');
+            $('#custom').append(chartDiv);
+            var customChart = echarts.init(chartDiv[0], 'chalk');
+            customChart.showLoading();
+
+            var chartSeries = [];
+            var cfg = DRAW.info.custom.cfgs[i];
+            var start = 0;
+            for (var j = 0; j < group.length; j++){
+                if (group[j] != undefined){
+                    chartSeries.push({
+                        name: cfg.series[j - start].name,
+                        type: 'line',
+                        smooth: false,
+                        data: group[j]
+                    });
+                }
+                else{
+                    start = j + 1;
+                }
+            }
+
+            customChart.setOption(option = {
+                tooltip: {
+                  trigger: 'axis',
+                  /*
+                  formatter: function(params){
+                      data = params[0].data
+                      return '' + data[0].toLocaleString('chinese',{hour12:false}) + 
+                            '<br/>' + 
+                            data[1]
+                  }
+                  */
+                 axisPointer: { type: 'cross' }
+                },
+                
+                title: {
+                  show: true,
+                  left: 'center',
+                  text: cfg.title.text
+                },
+                toolbox: {
+                  feature: {
+                    dataZoom: {
+                      yAxisIndex: 'none'
+                    },
+                    restore: {},
+                    saveAsImage: {}
+                  }
+                },
+                xAxis: {
+                  type: 'time',
+                  boundaryGap: false,
+                },
+                yAxis: {
+                  type: 'value',
+                  boundaryGap: [0, '100%'],
+                },
+                dataZoom: [
+                  {
+                    type: 'inside',
+                    start: 0,
+                    end: 100
+                  },
+                  {
+                    start: 0,
+                    end: 20
+                  }
+                ],
+                series: chartSeries
+            });
+            customChart.hideLoading();
+        }
+    })
+}
+
+DRAW.setLog = function(){
+    var logDiv = $('#log');
+    DRAW.logData.forEach(line => {
+        var logItem = $('<div class="log_item">' + 
+            '<i>' + 
+            line[0].toLocaleString('chinese',{hour12:false}) + ' ' + line[1] + 
+            '</i>' +
+            '<br>' + 
+            '&emsp;&emsp;' + line[2] + 
+            '</div>');
+
+        logDiv.append(logItem);
+    });
+}
